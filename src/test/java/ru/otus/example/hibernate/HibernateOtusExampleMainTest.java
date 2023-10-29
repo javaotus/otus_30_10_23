eef87a6d67a5402882f12e1b17269ce3c27e42fc
Vladimir Andreev <vladimirandreev1987@mail.com>
2023-10-29 13:15:38 +0100

    Added. Core part.

--- src/test/java/ru/otus/example/hibernate/HibernateOtusExampleMainTest.java
+++ src/test/java/ru/otus/example/hibernate/HibernateOtusExampleMainTest.java
@@ -15,13 +15,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import ru.otus.example.hibernate.entities.City;
-import ru.otus.example.hibernate.entities.Email;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ResourceBundle;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(OrderAnnotation.class)
 public class HibernateOtusExampleMainTest {
 
     private static final Log LOGGER = LogFactory.getLog(MethodHandles.lookup().lookupClass());
@@ -30,7 +36,8 @@ public class HibernateOtusExampleMainTest {
     private Session session = null;
 
     @BeforeAll
-    static void setup(){
+    static void setup() {
+        ResourceBundle.clearCache();
         try {
             StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
                 .configure("hibernate-test.cfg.xml")
@@ -38,54 +45,108 @@ public class HibernateOtusExampleMainTest {
 
             Metadata metadata = new MetadataSources(standardRegistry)
                 .addAnnotatedClass(City.class)
-                .addAnnotatedClass(Email.class)
                 .getMetadataBuilder()
             .build();
 
             sessionFactory = metadata.getSessionFactoryBuilder().build();
+            LOGGER.info("The SessionFactory has been created!");
 
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
     }
 
+    @AfterAll
+    static void tear() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            LOGGER.info("The SessionFactory has been destroyed!");
+        }
+    }
+
     @BeforeEach
     void setupThis(){
         session = sessionFactory.openSession();
-        session.beginTransaction();
+        LOGGER.info("The session has been created!");
     }
 
     @AfterEach
     void tearThis(){
-        session.getTransaction().commit();
-    }
-
-    @AfterAll
-    static void tear(){
-        sessionFactory.close();
+        if (session != null) {
+            session.close();
+            LOGGER.info("The session has been closed!");
+        }
     }
 
     @Test
-    void mustInsertCityEntity() {
+    @Order(1)
+    public void mustInsertNewCity() {
+
+        LOGGER.info("Inserting the city ...");
+
+        session.beginTransaction();
+
         City city = new City();
         city.setName("Chicago");
-        Assertions.assertNull(city.getId());
 
         session.persist(city);
-        LOGGER.info("The city with id : " + city.getId() + " has been inserted!");
-        Assertions.assertNotNull(city.getId());
+
+        session.getTransaction().commit();
+
+        assertEquals(1, city.getId());
+
     }
 
     @Test
-    void mustInsertEmailEntity() {
-        Email email = new Email();
-        email.setInbox("super@inbox.com");
+    @Order(2)
+    public void mustSelectExistingCity() {
 
-        Assertions.assertNull(email.getId());
+        LOGGER.info("Retrieving the city ...");
 
-        session.persist(email);
-        LOGGER.info("The email with id : " + email.getId() + " has been inserted!");
-        Assertions.assertNotNull(email.getId());
+        Integer id = 1;
+
+        City city = session.find(City.class, id);
+
+        assertEquals("Chicago", city.getName());
+
+    }
+
+    @Test
+    @Order(3)
+    public void mustUpdateExistingCity() {
+
+        Integer id = 1;
+        String updatedName = "Dallas";
+
+        LOGGER.info("Updating the city ...");
+
+        City city = session.find(City.class, id);
+        city.setName(updatedName);
+
+        session.beginTransaction();
+        session.merge(city);
+        session.getTransaction().commit();
+
+        City updatedCity = session.find(City.class, id);
+
+        assertEquals(updatedName, updatedCity.getName());
+    }
+
+    @Test
+    @Order(4)
+    public void mustDeleteExistingCity() {
+        LOGGER.info("Deleting the city ...");
+
+        Integer id = 1;
+        City city = session.find(City.class, id);
+
+        session.beginTransaction();
+        session.remove(city);
+        session.getTransaction().commit();
+
+        City deletedCity = session.find(City.class, id);
+
+        Assertions.assertNull(deletedCity);
     }
 
 }
